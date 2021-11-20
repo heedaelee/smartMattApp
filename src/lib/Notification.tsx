be6524fde931903fbb73a6143772dd0c91636ae5
heedaelee <adguy72@gmail.com>
2021-11-20 22:30:16 +0900

    push 작업중

--- src/lib/Notification.tsx
+++ src/lib/Notification.tsx
@@ -2,7 +2,40 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { AppState, PushNotificationIOS } from 'react-native';
+import {AppState, PushNotificationIOS} from 'react-native';
 import PushNotification, {Importance} from 'react-native-push-notification';
 
+export default {
+  register: async () => {
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        //토큰 출력위해
+        console.log('TOKEN:', token);
+      },
 
+      // (required) Called when a remote is received or opened, or local notification is opened
+      onNotification: function (notification) {
+        console.log('NOTIFICATION:', notification);
+        // process the notification
+
+        // (required) Called when a remote is received or opened, or local notification is opened
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      popInitialNotification: true,
+    });
+  },
+  createDefaultChannels: () => {
+    PushNotification.createChannel(
+      {
+        channelId: 'default-channel-id', // (required)
+        channelName: `Default channel`, // (required)
+        channelDescription: 'A default channel', // (optional) default: undefined.
+        soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+        importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+        vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+      },
+      created => console.log(`createChannel 'default-channel-id' returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+    );
+  },
+};
