3e0ba53125a44f0b91a7798721d2364ce56f772d
heedaelee <adguy72@gmail.com>
2021-11-20 22:29:40 +0900

    push 작업중

---
+++ src/lib/Notification.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { AppState, PushNotificationIOS } from 'react-native';
+import PushNotification, {Importance} from 'react-native-push-notification';
+
+
